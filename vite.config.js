44c56f5e9c3e922a2f517d019341330b3c0fbf00
Jhuliño A. Oroya Barreto <jaob.4.95@gmail.com>
2025-06-19 00:02:38 -0500

    Feat: add new url

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
-  base: "/ms-colors-g9/",
+  base: "/ms-colors-app/",
 });
